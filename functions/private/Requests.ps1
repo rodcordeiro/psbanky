dc15c59772480a24bf96d3448fa099c79e37949e
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-04-16 18:45:31 -0300

    refactor: Reiniciando módulo com melhorias

--- functions/private/Requests.ps1
+++ functions/private/Requests.ps1
@@ -1,30 +1,25 @@
-function Invoke-Api {
+﻿function Invoke-Api {
     param(
         [uri]$Uri,
         [string]$Method,
         [AllowNull()][Object]$Body
     )
     begin {
-        if (!(Test-Path "$($(Resolve-Path -Path $env:USERPROFILE).Path)\.banky" -ErrorAction Stop)) {
-            throw "Banky not found. Please configure it."
-        }
-        [BankyAuthenticationResponse]$bankyAuth = $(Get-Content "$($(Resolve-Path -Path $env:USERPROFILE).Path)\.banky" -ErrorAction Stop | ConvertFrom-Json )
-
-        $isExpired = [datetime]::Parse($bankyAuth.expirationDate) -lt [DateTime]::Now
-
-        if ($isExpired) {
-            try {
-                $cred = $bankyAuth.GetCredentials()
-                New-BankyAuthentication @cred
+        try {
+            $cred = Get-StoredCredential -Target "BANKY"
+            if (-not $cred) {
+                throw "Credenciais não encontradas. Reautentique."
             }
-            catch {
-                Throw "Login expirado, autentique novamente"
-            }
+            Invoke-Authentication -username $Cred.username -Password (Protect-String $Cred.GetNetworkCredential().password)
         }
+        catch {
+            Throw "Login expirado. Reautentique."
+        }
+
 
         $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
         $headers.Add("Content-Type", "application/json")
-        $headers.Add("Authorization", "Bearer $($bankyAuth.accessToken)")
+        $headers.Add("Authorization", "Bearer $($script:BANKY_AUTH_TOKEN)")
 
     }
     process {
--- functions/public/Get-BankyAccount.ps1
+++
@@ -1,9 +0,0 @@
-﻿function Get-BankyAccount {
-    [CmdletBinding()]
-    param ()
-
-    process {
-        [BankyAccount[]]$response = Invoke-Api 'http://82.180.136.148:3338/api/v1/accounts' -Method 'GET'
-        return $response
-    }
-}
